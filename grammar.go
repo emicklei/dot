99d1a5f0793df7ac02d6c6bf5224c2858d91733e
Ernest Micklei <emicklei@Ernests-iMac.local>
2015-12-30 18:30:49 +0100

    some tests, cleanup output

--- grammar.go
+++ grammar.go
@@ -5,22 +5,27 @@ import (
 	"fmt"
 )
 
-type attributeList struct {
+// AttributesMap holds attribute=value pairs.
+type AttributesMap struct {
 	attributes map[string]interface{}
 }
 
-func (a attributeList) Attr(label string, value interface{}) {
+// Attr sets the value for an attribute (unless empty).
+func (a AttributesMap) Attr(label string, value interface{}) {
+	if len(label) == 0 {
+		return
+	}
 	a.attributes[label] = value
 }
 
 type node struct {
-	attributeList
+	AttributesMap
 	id  string
 	seq int
 }
 
 type edge struct {
-	attributeList
+	AttributesMap
 	from, to node
 }
 
@@ -37,37 +42,59 @@ func NewDigraph() *Digraph {
 	}
 }
 
+// Node creates a new with label set to id
 func (g *Digraph) Node(id string) node {
 	g.seq++
-	return node{id: id, seq: g.seq, attributeList: attributeList{attributes: map[string]interface{}{
+	return node{id: id, seq: g.seq, AttributesMap: AttributesMap{attributes: map[string]interface{}{
 		"label": id,
 	}}}
 }
 
+// Node creates a new with from set to n1 and to set to n2
 func (g Digraph) Edge(n1, n2 node) edge {
-	e := edge{from: n1, to: n2, attributeList: attributeList{attributes: map[string]interface{}{}}}
+	e := edge{from: n1, to: n2, AttributesMap: AttributesMap{attributes: map[string]interface{}{}}}
 	g.nodes[n1.id] = n1
 	g.nodes[n2.id] = n2
 	g.edgesFrom[n1.id] = e
 	return e
 }
 
+// String returns the source in dot notation.
 func (g Digraph) String() string {
 	b := new(bytes.Buffer)
 	b.WriteString("digraph {\n")
 	for _, each := range g.nodes {
-		fmt.Fprintf(b, "\tnode [")
-		for label, value := range each.attributes {
-			fmt.Fprintf(b, "%s=%q,", label, value)
+		fmt.Fprintf(b, "\tnode")
+		if len(each.attributes) > 0 {
+			b.WriteString(" [")
+			first := true
+			for label, value := range each.attributes {
+				if !first {
+					fmt.Fprintf(b, ", ")
+				}
+				fmt.Fprintf(b, "%s=%q", label, value)
+				first = false
+			}
+			b.WriteString("]")
 		}
-		fmt.Fprintf(b, "]; n%d;\n", each.seq)
+		fmt.Fprintf(b, "; n%d;\n", each.seq)
 	}
 	for _, each := range g.edgesFrom {
-		fmt.Fprintf(b, "\tn%d -> n%d [", each.from.seq, each.to.seq)
-		for label, value := range each.attributes {
-			fmt.Fprintf(b, "%s=%q,", label, value)
+		fmt.Fprintf(b, "\tn%d -> n%d", each.from.seq, each.to.seq)
+		if len(each.attributes) > 0 {
+			b.WriteString(" [")
+			first := true
+			for label, value := range each.attributes {
+				if !first {
+					fmt.Fprintf(b, ", ")
+				}
+				fmt.Fprintf(b, "%s=%q", label, value)
+				first = false
+			}
+			b.WriteString("]")
 		}
-		b.WriteString("];\n")
+		b.WriteString(";\n")
+
 	}
 	b.WriteString("}")
 	return b.String()
--- grammar_test.go
+++ grammar_test.go
@@ -1,1 +1,26 @@
 package dot
+
+import "testing"
+
+func TestEmpty(t *testing.T) {
+	di := NewDigraph()
+	if got, want := di.String(), `digraph {
+}`; got != want {
+		t.Fail()
+	}
+}
+
+func TestTwoConnectedNodes(t *testing.T) {
+	di := NewDigraph()
+	n1 := di.Node("A")
+	n2 := di.Node("B")
+	di.Edge(n1, n2)
+	if got, want := di.String(), `digraph {
+	node [label="A"]; n1;
+	node [label="B"]; n2;
+	n1 -> n2;
+}`; got != want {
+		println(got)
+		t.Fail()
+	}
+}
