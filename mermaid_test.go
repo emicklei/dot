408fc8627d3ad7ca6fa5cdb338d9bf114b4dfa76
Ernest Micklei <ernest.micklei@gmail.com>
2022-10-03 09:46:29 +0200

    fix test

--- mermaid_test.go
+++ mermaid_test.go
@@ -2,14 +2,12 @@ package dot
 
 import (
 	"testing"
-
-	"github.com/emicklei/dot"
 )
 
 func TestMermaidSimple(t *testing.T) {
-	di := dot.NewGraph(dot.Directed)
+	di := NewGraph(Directed)
 	n1 := di.Node("e1").Label("E1")
 	n2 := di.Node("e2")
 	n1.Edge(n2, "what").Attr("x", "y")
-	t.Log(Graph(di, TopDown))
+	t.Log(MermaidGraph(di, MermaidTopDown))
 }
